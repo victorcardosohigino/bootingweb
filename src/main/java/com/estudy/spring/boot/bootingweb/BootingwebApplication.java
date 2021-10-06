733ad5e89f788dcbc1e5f88ad5d0da5b7b10d312
Victor Cardoso Higino <victor.higino@gam.com.br>
2021-10-06 10:43:22 -0300

    first commit

---
+++ src/main/java/com/estudy/spring/boot/bootingweb/BootingwebApplication.java
@@ -0,0 +1,13 @@
+package com.estudy.spring.boot.bootingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootingwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootingwebApplication.class, args);
+	}
+
+}
